f4ac6d38746bec6e6a4bd839dc4e7ff2b0accf9e
Manu <mcabral@frba.utn.edu.ar>
2021-09-17 20:25:46 -0300

    added client windows

---
+++ client_windows.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <winsock.h>
+
+// Winsock library
+#pragma comment(lib, "ws2_32.lib")
+
+// execute: gcc server_windows.c -lws2_32 -o server_windows
+
+int main(int argc, char *argv[])
+{
+    SOCKET so;
+    WSADATA wsa;
+
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+    {
+        printf("Error: %d", WSAGetLastError());
+        return 1;
+    }
+
+    printf("Winsock Initialised.");
+
+    /**
+     * @brief Creates a socket and return socket descriptor
+     * AF_INET: IP Version 4 (Address Family)
+     * SOCK_STREAM: Oriented to TCP protocol (Type)
+     * 0: (Protocol)
+     */
+    if((so = socket(AF_INET , SOCK_STREAM , 0 )) == INVALID_SOCKET)
+	{   
+		printf("Error on create: %d" , WSAGetLastError());
+        return 1;
+	}
+
+    printf("Socket created.");
+
+    closesocket(so);
+    WSACleanup();   
+    return 0;
+}
