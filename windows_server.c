3cfd5e0b38cce517d17be4d84dc3d94251c93f45
Manu <mcabral@frba.utn.edu.ar>
2021-09-20 13:42:32 -0300

    handle client updated

--- windows_server.c
+++ windows_server.c
@@ -1,16 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <winsock.h>
 
 // Winsock library
 #pragma comment(lib, "ws2_32.lib")
 
 #define MAX_PENDING 5
+#define RECEIVE_BUFFER_SIZE 32
+
+void error(char *message)
+{
+    fprintf(stderr, "%s: %d\n", message, WSAGetLastError());
+    exit(1);
+    return;
+}
 
 void close(SOCKET so)
 {
     closesocket(so);
     WSACleanup();
     return;
+}
+
+void handle_client(int client_socket)
+{
+    char buffer[RECEIVE_BUFFER_SIZE];
+    char message[200] = "Welcome to the server!\n";
+    int receive_message_size;
+
+    send(client_socket, message, strlen(message) + 1, 0);
+
+    if ((receive_message_size = recv(client_socket, buffer, RECEIVE_BUFFER_SIZE, 0)) < 0)
+        error("Error on receive data.\n");
+
+    while (receive_message_size > 0)
+    {
+        //if (send(client_socket, buffer, receive_message_size, 0) != receive_message_size)
+            //error("Send message failed.\n");
+        buffer[receive_message_size] = '\0';
+        printf("[TCP Client]: %s\n", buffer);
+
+        scanf("%s", message);
+        printf("[Server]: %s\n", message);
+        send(client_socket, message, strlen(message) + 1, 0);
+
+        if ((receive_message_size = recv(client_socket, buffer, RECEIVE_BUFFER_SIZE, 0)) < 0)
+            error("Error on receive data.\n");
+    }
+
+    puts("Closing client socket..");
+    closesocket(client_socket);
 }
 
 int main(int argc, char *argv[])
@@ -33,18 +72,12 @@ int main(int argc, char *argv[])
     port = atoi(argv[2]);
 
     if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
-    {
-        printf("Error: %d\n", WSAGetLastError());
-        return 1;
-    }
+        error("Failed WSAStartup.\n");
 
     printf("Winsock Initialised.\n");
 
     if ((server_so = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET)
-    {
-        printf("Error on create: %d\n", WSAGetLastError());
-        return 1;
-    }
+        error("Failed socket.\n");
 
     printf("Socket created.\n");
 
@@ -53,23 +86,24 @@ int main(int argc, char *argv[])
     server.sin_addr.s_addr = inet_addr(ip);
     server.sin_port = htons(port);
 
-    if (bind(server_so, (struct sockaddr *)&server, sizeof(server)) == SOCKET_ERROR){
-        printf("Bind failed: %d", WSAGetLastError());
-        close(server_so);
-        return 1;
+    if (bind(server_so, (struct sockaddr *)&server, sizeof(server)) == SOCKET_ERROR)
+        error("Bind failed.\n");
+
+    if (listen(server_so, MAX_PENDING) < 0)
+        error("Listen failed.\n");
+
+    puts("Waiting for incoming connections..");
+
+    while (1)
+    {
+        client_length = sizeof(client);
+
+        if ((client_so = accept(server_so, (struct sockaddr *)&client, &client_length)) < 0)
+            error("Error on accept.\n");
+
+        printf("New connection accepted %s\n", inet_ntoa(client.sin_addr));
+        handle_client(client_so);
     }
-
-    if (listen(server_so, MAX_PENDING) < 0){
-        printf("Listen failed: %d", WSAGetLastError());
-        close(server_so);
-        return 1;
-    }
-
-    printf("Waiting for incoming connections..");
-
-    client_length = sizeof(client);
-    client_so = accept(server_so, (struct sockaddr *)&client, &client_length);
-    puts("Connection accepted");
 
     close(server_so);
     return 0;
